f076252654feb0a2ea6b424132a8b33c86ff478e
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2026-09-03 11:43:52 +0700

    fix build on stm32 main_usb_serial

--- firmware_kitchen/stm32/src/bin/main_usb_serial.rs
+++ firmware_kitchen/stm32/src/bin/main_usb_serial.rs
@@ -1,29 +1,139 @@
 #![no_std]
 #![no_main]
 
+use embassy_futures::join::join;
+use embassy_usb::{
+    Builder,
+    class::cdc_acm::{CdcAcmClass, State},
+    driver::EndpointError,
+};
+use kitchen_stm32::init_alloc;
+
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
-use kitchen_stm32::init_alloc;
+use embassy_stm32::{
+    Config, bind_interrupts, peripherals,
+    time::Hertz,
+    usb::{self, Driver, Instance},
+};
 use panic_probe as _;
+
+bind_interrupts!(struct Irqs {
+    OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     init_alloc();
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(8_000_000),
+            mode: HseMode::Bypass,
+        });
+        // config.rcc.pll_src = PllSource::Hse;
+        // config.rcc.pll = Some(Pll {
+        //     prediv: PllPreDiv::Div4,
+        //     mul: PllMul::Mul168,
+        //     divp: Some(PllPDiv::Div2), // 8mhz / 4 * 168 / 2 = 168Mhz.
+        //     divq: Some(PllQDiv::Div7), // 8mhz / 4 * 168 / 7 = 48Mhz.
+        //     divr: None,
+        // });
+        // config.rcc.ahb_pre = AHBPrescaler::Div1;
+        // config.rcc.apb1_pre = APBPrescaler::Div4;
+        // config.rcc.apb2_pre = APBPrescaler::Div2;
+        // config.rcc.sys = Sysclk::Pll1P;
+        // config.rcc.mux.clk48sel = mux::Clk48sel::Pll1Q;
+    }
+    let p = embassy_stm32::init(config);
 
-    let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+    // Create the driver, from the HAL.
+    let mut ep_out_buffer = [0u8; 256];
+    let mut config = embassy_stm32::usb::Config::default();
 
+    // Do not enable vbus_detection. This is a safe default that works in all boards.
+    // However, if your USB device is self-powered (can stay powered on if USB is unplugged), you need
+    // to enable vbus_detection to comply with the USB spec. If you enable it, the board
+    // has to support it or USB won't work at all. See docs on `vbus_detection` for details.
+    config.vbus_detection = false;
+
+    let driver = Driver::new_fs(
+        p.USB_OTG_FS,
+        Irqs,
+        p.PA12,
+        p.PA11,
+        &mut ep_out_buffer,
+        config,
+    );
+
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
+
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    // Do stuff with the class!
+    let echo_fut = async {
+        loop {
+            class.wait_connection().await;
+            info!("Connected");
+            let _ = echo(&mut class).await;
+            info!("Disconnected");
+        }
+    };
+
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, echo_fut).await;
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => defmt::panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: Instance + 'd>(
+    class: &mut CdcAcmClass<'d, Driver<'d, T>>,
+) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
     loop {
-        info!("led on!");
-        led.set_high();
-        Timer::after_millis(500).await;
-
-        info!("led off!");
-        led.set_low();
-        Timer::after_millis(500).await;
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        info!("data: {:x}", data);
+        class.write_packet(data).await?;
     }
 }
